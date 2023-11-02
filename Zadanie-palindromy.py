5b61ca2c9bdd846233efb84bcc73907ba3802eb1
Michal Lewandowski <michalewy00@gmail.com>
2023-11-02 20:14:57 +0100

    Dodanie listy Words_list

---
+++ Zadanie-palindromy.py
@@ -0,0 +1,12 @@
+Words_list = [
+    "kajak",
+    "potop",
+    "słoń",
+    "anna",
+    "komputer",
+    "python",
+    "jest",
+    "super",
+]
+
+def Funkcja_sprawdzająca (first_word, second_word):
